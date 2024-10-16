b80f02d25c6cbd725361db1aa878efc1c0300781
Yipmong Said <Syipmong5@gmail.com>
2024-10-16 14:49:26 +0100

    Refactor blockchain.py to include transaction functionality and mining rewards

--- blockchain.py
+++ blockchain.py
@@ -1,62 +1,72 @@
 import hashlib
 import time
 
+import transaction
 
 class Block:
-    def __init__(self, index, previous_hash, data, timestamp=None, difficulty=4):
+    def __init__(self, index, previous_hash, data, transactions, difficulty=4):
         self.index = index
         self.previous_hash = previous_hash
         self.data = data
-        self.timestamp = timestamp or time.time()
+        self.transactions = transactions
+        self.timestamp = time.time()
         self.nonce = 0
         self.difficulty = difficulty
         self.hash = self.calculate_hash()
-        
 
     def calculate_hash(self):
-        block_string = f"{self.index}{self.previous_hash}{self.data}{self.timestamp}{self.nonce}"
+        transaction_string = "".join([str(tx.__dict__) for tx in self.transactions])
+        block_string = f"{self.index}{self.previous_hash}{self.data}{self.timestamp}{self.nonce}{transaction_string}"
         return hashlib.sha256(block_string.encode()).hexdigest()
-    
+
     def mine_block(self):
         while self.hash[:self.difficulty] != '0' * self.difficulty:
             self.nonce += 1
             self.hash = self.calculate_hash()
         print(f"Block Mined: {self.hash}")
-    
+
 
 class BlockChain:
     def __init__(self):
+        self.difficulty = 4
         self.chain = [self.create_first_block()]
+        self.pending_transactions = []
+        self.mining_reward = 100
 
     def create_first_block(self):
-        first_block = Block(0,'0', 'First Block')
+        first_block = Block(0, '0', 'First Block', [], self.difficulty)
         first_block.mine_block()
         return first_block
-    
+
+    def add_transactions(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def mine_pending_transactions(self, miner_address):
+        reward_transaction = transaction.Transaction('System', miner_address, self.mining_reward)
+        self.pending_transactions.append(reward_transaction)
+        new_block = Block(len(self.chain), self.get_last_block().hash, 'Pending Transactions', self.pending_transactions, self.difficulty)
+
+        new_block.mine_block()
+        self.chain.append(new_block)
+
+        self.pending_transactions = []
+
     def get_last_block(self):
         return self.chain[-1]
-    
+
     def add_block(self, new_block):
         new_block.previous_hash = self.get_last_block().hash
         new_block.mine_block()
         self.chain.append(new_block)
 
-
     def is_chain_valid(self):
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
-            previous_block = self.chain[i-1]
+            previous_block = self.chain[i - 1]
             if current_block.hash != current_block.calculate_hash():
                 return False
-            
+
             if current_block.previous_hash != previous_block.hash:
                 return False
-            
+
         return True
-        
-
-
-
-
-
-        
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from blockchain import *
+from transaction import *
 
 if __name__ == '__main__':
 
     blockchain = BlockChain()
 
-    blockchain.add_block(Block(1,blockchain.get_last_block().hash,'Block 1 Data'))
-    blockchain.add_block(Block(2,blockchain.get_last_block().hash,'Block 2 Data'))
+    blockchain.add_transactions(Transaction('Yipmong', 'Said', 30))
+    blockchain.add_transactions(Transaction('Said', 'Yipmong', 20))
+    blockchain.add_transactions(Transaction('John', 'Said', 10))
+    blockchain.add_transactions(Transaction('Nelson', 'Hosiah', 5))
+
+    print('Mining new block ...')
+
+    blockchain.mine_pending_transactions('Yipmong')
 
     for block in blockchain.chain:
-        print(f"Block {block.index} Block Data: {block.data} Hash: {block.hash} Nonce: {block.nonce}")
+        print(f"Block {block.index}:")
+        for tx in block.transactions:
+            print(f"{tx.sender} sent {tx.amount} to {tx.recipient}")
 
-    print(f"Is the blockchain Valid? {blockchain.is_chain_valid()}")
+    print(f"Is the BlockChain Valid? {blockchain.is_chain_valid()}")
