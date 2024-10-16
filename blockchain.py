6d4e7c635ace9f43acdc636112a6465adc75cc1c
Yipmong Said <Syipmong5@gmail.com>
2024-10-16 13:34:41 +0100

    Refactor blockchain.py to add block validation and chain integrity checks

--- blockchain.py
+++ blockchain.py
@@ -20,5 +20,29 @@ class BlockChain:
         self.chain = [self.create_first_block()]
 
     def create_first_block(self):
-        pass
+        return Block(0,'0','First block')
+    
+    def get_last_block(self):
+        return self.chain[-1]
+    
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_last_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            
+            if current_block.previous_hash != previous_block.hash:
+                return False
+            
+        return True
+        
+
+    
         
